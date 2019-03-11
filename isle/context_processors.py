626fec860d824f1ed0eed0492e79cfcb8b456189
nosanity <no-sanity17@ya.ru>
2019-03-11 19:13:32 +0300

    UPLOADS-165 Пустые строки в селекте контекстов

--- isle/context_processors.py
+++ isle/context_processors.py
@@ -4,7 +4,7 @@ from isle.models import Context
 def context(request):
     contexts = []
     if request.user.is_authenticated and request.user.is_assistant:
-        contexts = Context.objects.values_list('id', 'title')
+        contexts = ((i[0], i[1] or i[2] or i[3]) for i in Context.objects.values_list('id', 'title', 'guid', 'uuid'))
     return {
         'AVAILABLE_CONTEXTS': contexts,
     }
