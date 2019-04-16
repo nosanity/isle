d1a4c4e2e31d1f0e892ad8e8ddca7789679bb8cb
nosanity <nosanity17@gmail.com>
2019-04-16 12:59:03 +0300

    UPLOADS-194 Сделать опциональными параметры unti_id / team_id

--- isle/filters.py
+++ isle/filters.py
@@ -4,7 +4,7 @@ from isle.models import LabsUserResult, LabsTeamResult
 
 class LabsUserResultFilter(django_filters.FilterSet):
     created_at = django_filters.IsoDateTimeFromToRangeFilter(field_name='eventmaterial__created_at')
-    unti_id = django_filters.NumberFilter(field_name='user__unti_id')
+    unti_id = django_filters.NumberFilter(field_name='user__unti_id', required=False)
 
     class Meta:
         model = LabsUserResult
@@ -13,7 +13,7 @@ class LabsUserResultFilter(django_filters.FilterSet):
 
 class LabsTeamResultFilter(django_filters.FilterSet):
     created_at = django_filters.IsoDateTimeFromToRangeFilter(field_name='eventteammaterial__created_at')
-    team_id = django_filters.NumberFilter()
+    team_id = django_filters.NumberFilter(required=False)
 
     class Meta:
         model = LabsTeamResult
