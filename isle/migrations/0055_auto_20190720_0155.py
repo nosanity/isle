90bdd2b9e5ab107121c79d067c9a35affa882007
nosanity <nosanity17@gmail.com>
2019-08-08 16:53:49 +0300

    UPLOADS-257 Ошибка скрипта в миграции

--- isle/migrations/0055_auto_20190720_0155.py
+++ isle/migrations/0055_auto_20190720_0155.py
@@ -2,7 +2,28 @@
 
 from django.db import migrations, models
 import django.db.models.deletion
-from isle.utils import create_circle_items_for_result
+
+
+def create_circle_items_for_result(result_id, current_circle_items_ids, meta, metamodels, competences, CircleItem):
+    real_items_ids = []
+    for meta_item in meta:
+        tools = meta_item.get('tools')
+        if not isinstance(tools, list):
+            tools = [None]
+        for tool in tools:
+            circle_item = CircleItem.objects.get_or_create(
+                level=meta_item.get('level'),
+                sublevel=meta_item.get('sublevel'),
+                competence_id=competences.get(meta_item.get('competence')),
+                model_id=metamodels.get(meta_item.get('model')),
+                result_id=result_id,
+                tool=tool,
+            )[0]
+            real_items_ids.append(circle_item.id)
+    deleted_circle_items = set(current_circle_items_ids) - set(real_items_ids)
+    if deleted_circle_items:
+        CircleItem.objects.filter(id__in=deleted_circle_items).delete()
+    return real_items_ids
 
 
 def fix_circle_items(apps, schema):
@@ -16,6 +37,7 @@ def fix_circle_items(apps, schema):
     LabsEventResult = apps.get_model('isle', 'LabsEventResult')
     LabsUserResult = apps.get_model('isle', 'LabsUserResult')
     LabsTeamResult = apps.get_model('isle', 'LabsTeamResult')
+    CircleItem = apps.get_model('isle', 'CircleItem')
     competence_uuid_to_id = dict(DpCompetence.objects.values_list('uuid', 'id'))
     model_uuid_to_id = dict(MetaModel.objects.values_list('uuid', 'id'))
     for res in LabsEventResult.objects.filter(meta__isnull=False):
@@ -25,7 +47,8 @@ def fix_circle_items(apps, schema):
                 list(res.circle_items.values_list('id', flat=True)),
                 res.meta,
                 model_uuid_to_id,
-                competence_uuid_to_id
+                competence_uuid_to_id,
+                CircleItem,
             )
             for result_model in (LabsUserResult, LabsTeamResult):
                 for result in result_model.objects.filter(result=res).iterator():
